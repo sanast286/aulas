0d544a0daafa99d8e3ba7fc9c3e19d63a29a2d9f
erbernardino <emerson.rocco@gmail.com>
2024-03-05 18:54:51 -0300

    crud simples

--- dia 1/log.js
+++
@@ -1,1 +0,0 @@
-console.log('Boa Noite Abc Aprendiz')
---
+++ dia2/crud1/frontend/crud1/src/components/Crud.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        
+    <table border="0">
+        <tr><td>Nome</td><td><input v-model="pessoaForm.nome"> </td></tr>
+        <tr><td>Idade</td><td><input v-model="pessoaForm.idade"> </td></tr>
+        <tr><td><Button @click="grava(pessoaForm)">Gravar</Button></td></tr>
+    </table>
+  
+  <hr>
+  <h2>Lista de pessoas</h2>
+  <table border="2" width="60%">
+    <tr><td>Indice</td><td>Nome</td><td>Idade</td><td>Ações</td></tr>
+    <tr v-for="(pessoa,indice) in pessoas" :key="indice">
+        <td>{{ indice }}</td>
+        <td>{{ pessoa.nome }}</td>
+        <td>{{ pessoa.idade }}</td>
+        <td><button @click="remove(indice)">Remover</button><button @click="editar(indice)">Editar</button></td>
+    </tr>
+  </table>
+    </div>
+
+
+</template>
+
+<script>
+export default {
+    
+    name: 'CrudPage',
+    data() {
+        return {
+            
+            pessoaForm:{nome:'', idade: ''},
+            pessoas:[]
+        }
+    },
+    methods: {
+        grava(pessoaForm){
+            var pessoa = { ...pessoaForm}
+            this.pessoas.push(pessoa)
+            this.pessoaForm = {nome:'', idade: ''}
+        },
+        remove(indice){
+            this.pessoas.splice(indice,1)
+        },
+        editar(indice){
+            this.pessoaForm = this.pessoas[indice]
+            this.remove(indice)
+        },
+    },
+
+}
+</script>
+
+<style>
+
+</style>
