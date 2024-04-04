6228ab153584fe66a5163b49583454a0433edb74
fc202401 <161753873+fc202401@users.noreply.github.com>
2024-04-04 20:08:16 +0000

    dia6

--- dia6/backend-2/app.js
+++ dia6/backend-2/app.js
@@ -6,26 +6,52 @@ const cors =  require('cors');
 const PORT = 4001;
 
 var app = express();
-app.use(express.json);
+app.use(express.json());
 
 
 app.listen(PORT , () => {
     console.log('Servidor escutando a port ' + PORT);
 })
 
+var aprendizes = [
 
-var aprendizes = []
+    {
+        "id": 1,
+        "nome": "Emerson",
+        "cidade": "Santo André"
+    },
+    {
+        "id": 2,
+        "nome": "Joao",
+        "cidade": "Santo André"
+    },
+    {
+        "id": 3,
+        "nome": "Maria",
+        "cidade": "Santos"
+    }
+]
+
+
 
 app.get( '', (req, res) => {
     res.status(200).json(aprendizes);
     
 })
 
+app.get('/:id', (req, res) => {
+    const { id } = req.params;
+    res.status(200).json(aprendizes.filter( aprendiz => aprendiz.id === parseInt(id) ))
+})
+
 
 
 app.post('', (req, res) => {
     
-    console.log(req)
+    
     aprendizes.push(req.body)
     res.status(201).json(aprendizes)
 })
+
+
+
---
+++ dia6/backend-5/src/rotas.js
@@ -0,0 +1,24 @@
+const express =  require('express');
+const AprendizController = require('./aprendiz')
+const EmpresaController = require('./empresa')
+const router = express.Router();
+
+
+//Aprendiz
+router.get( '/aprendiz', AprendizController.listar)
+router.get('/aprendiz/:id',AprendizController.obter)
+router.post('/aprendiz',AprendizController.criar)
+router.delete("/aprendiz/:id ", AprendizController.apagar)
+
+
+
+
+//Empresas
+router.get( '/empresa', EmpresaController.listar)
+router.get('/empresa/:id',EmpresaController.obter)
+router.post('/empresa',EmpresaController.criar)
+router.delete("/empresa/:id ", EmpresaController.apagar)
+
+
+
+module.exports = router;
