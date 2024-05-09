262bcf463a179bdf69d77c80fe21fb35d1770eb2
erbernardino <emerson.rocco@gmail.com>
2024-05-09 16:35:23 -0300

    frontend-basico

---
+++ dia10/frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <AprendizView /><hr>
+    <EmpresaView />
+  </div>
+
+</template>
+
+<script>
+import AprendizView from './components/AprendizView.vue';
+import EmpresaView from './components/EmpresaView.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    AprendizView,
+    EmpresaView
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
